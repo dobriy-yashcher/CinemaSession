71fcc40ce10636f46ba4e331ef7406fecd58e4b4
dobriy_yashcher <golovinsergey17@gmail.com>
2023-04-21 00:19:49 +0300

    Added authorization

--- CinemaSessionWPF/PagesApp/AuthorizationPage.xaml.cs
+++ CinemaSessionWPF/PagesApp/AuthorizationPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CinemaSessionWPF.ADOApp;
+using CinemaSessionWPF.Components;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CinemaSessionWPF.PagesApp
 {
@@ -23,6 +15,40 @@ namespace CinemaSessionWPF.PagesApp
         public AuthorizationPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder ();
+
+            /// <summary>
+            /// Проверка данных
+            /// </summary>
+            if (string.IsNullOrWhiteSpace(TbLogin.Text))
+                errors.AppendLine("Введите логин");   
+            if (string.IsNullOrWhiteSpace(TbPassword.Password))
+                errors.AppendLine("Введите пароль");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var findUser = CinemaSessionEntities
+                .GetContext().User
+                .Where(x => x.Login == TbLogin.Text)
+                .FirstOrDefault();
+
+            if (findUser == null || findUser.Password != TbPassword.Password)
+                MessageBox.Show("Не правильный логин или пароль", 
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+            {
+                MessageBox.Show("Авторизация прошла успешно!", 
+                    "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                Manager.MainFrame.Navigate(new MainPage());
+            }
         }
     }
 }
